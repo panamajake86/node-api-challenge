4690bfec787d638dbb677a4efa55b2d2cbe01886
Jake <panamajake86@gmail.com>
2020-01-24 18:34:28 -0700

    finished the first part of the project. Got the server.js and the index.js files sorted.  set up the folder and file for the projectsRouter. Got the nodemon and the node scripts setup in the package.json file.

---
+++ routes/projectsRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const Project = require('../data/helpers/projectModel');
+const Action = require('../data/helpers/actionModel');
+
+const router = express.Router();
+
+
+
+
+module.exports = router;
